f5350ce713c49dc0f2df563b3e2911bb6657d4c0
Garcipaldis <jgn.sando@gmail.com>
2021-07-06 19:55:08 +0200

    First GAN

--- Text_Generation_Project/src/utils/models.py
+++ Text_Generation_Project/src/utils/models.py
@@ -68,7 +68,7 @@ class CharacterPreprocessor:
         probas = np.random.multinomial(1, preds, 1)
         return np.argmax(probas)
 
-    def generate(self, model, quote_len=40, sentence=False, temperature=1.0, verbose=False, fake=False):
+    def generate(self, model, option=1, quote_len=40, sentence=False, temperature=1.0, verbose=False, fake=False):
         
         if verbose:
             print("...Temperature:", temperature)
@@ -83,15 +83,26 @@ class CharacterPreprocessor:
         if verbose:
             print('...Generating with seed: "' + sentence + '"')
 
-        for i in range(quote_len):
+        if option == 1:
+            for i in range(quote_len):
+                x_pred = np.zeros((1, self.maxlen, len(self.chars)))
+                for t, char in enumerate(sentence):
+                    x_pred[0, t, self.char_indices[char]] = 1.0 #One-Hot Array
+                preds = model.predict(x_pred, verbose=0)[0]
+                next_index = self.sample(preds, temperature)
+                next_char = self.indices_char[next_index]
+                sentence = sentence[1:] + next_char
+                generated += next_char
+
+        elif option == 2:
             x_pred = np.zeros((1, self.maxlen, len(self.chars)))
             for t, char in enumerate(sentence):
                 x_pred[0, t, self.char_indices[char]] = 1.0 #One-Hot Array
             preds = model.predict(x_pred, verbose=0)[0]
-            next_index = self.sample(preds, temperature)
-            next_char = self.indices_char[next_index]
-            sentence = sentence[1:] + next_char
-            generated += next_char
+            for pred in list(preds):
+                next_index = self.sample(pred, temperature=1)
+                next_char = self.indices_char[next_index]
+                generated += next_char
 
         if verbose:
             print("...Generated: ", generated)
@@ -102,14 +113,14 @@ class CharacterPreprocessor:
 
         x_random = []
         for n in range(n_samples):
-            x_random.append(self.generate(model, quote_len=quote_len, temperature=temperature, fake=True))
+            x_random.append(self.generate(model, option=2, quote_len=quote_len, temperature=temperature, fake=True))
 
         X_fake = np.zeros((len(x_random), self.maxlen, len(self.chars)))
         for i, sentence in enumerate(x_random):
             for t, char in enumerate(sentence):
                 X_fake[i, t, self.char_indices[char]] = 1
 
-        X_fake = X_fake.reshape(X_fake.shape[0], X_fake.shape[1], X_fake.shape[2], 1)
+        #X_fake = X_fake.reshape(X_fake.shape[0], X_fake.shape[1], X_fake.shape[2], 1)
         y_fake = np.zeros(n_samples)
 
         return X_fake, y_fake
@@ -117,7 +128,75 @@ class CharacterPreprocessor:
     def generate_real_samples(self, n_samples):
 
         X_true = np.array(random.sample(list(self.X), n_samples))
-        X_true = X_true.reshape(X_true.shape[0], X_true.shape[1], X_true.shape[2], 1)
+        #X_true = X_true.reshape(X_true.shape[0], X_true.shape[1], X_true.shape[2], 1)
         y_true = np.ones(n_samples)
 
         return X_true, y_true
+
+    def generate_gan_samples(self, n_samples):
+
+        sample_list = random.sample(range(len(self.X)), n_samples)
+
+        X_gan = self.X[sample_list]
+        y_gan = np.ones(len(X_gan))
+
+        return X_gan, y_gan
+
+    def generate_disc_inputs(self, model, n_samples, quote_len=40, temperature=1):
+
+        sample_list = random.sample(self.sentences, n_samples)
+
+        predictions = []
+        for seq in sample_list:
+            predictions.append(self.generate(model, quote_len=quote_len, temperature=temperature, sentence=seq))
+
+        # Defining X and y
+        x_input = np.zeros((len(predictions), self.maxlen, len(self.chars)))
+        for i, sentence in enumerate(predictions):
+            for t, char in enumerate(sentence):
+                x_input[i, t, self.char_indices[char]] = 1
+
+        return x_input
+
+    def preprocess_type2(self, maxlen=40, step=3, column='quote'):
+
+        self.maxlen = maxlen
+
+        # Quote List
+        input_list = list(self.data[column])
+
+        # Corpus
+        text = self.get_corpus(input_list)
+
+        # Total Characters
+        chars = sorted(list(set(text)))
+        print("Total chars:", len(chars))
+
+        # Dictionaries
+        char_indices = dict((c, i) for i, c in enumerate(chars))
+        indices_char = dict((i, c) for i, c in enumerate(chars))
+
+        # Number of sequences
+        sentences = []
+        next_seq = []
+        for i in range(0, len(text) - 2*maxlen, step):
+            sentences.append(text[i : i + maxlen])
+            next_seq.append(text[i + maxlen:i + 2*maxlen])
+        print("Number of sequences:", len(sentences))
+
+        # Defining X and y
+        x = np.zeros((len(sentences), maxlen, len(chars)))
+        y = np.zeros((len(sentences), maxlen, len(chars)))
+        for i, sentence in enumerate(sentences):
+            for t, char in enumerate(sentence):
+                x[i, t, char_indices[char]] = 1
+                y[i, t, char_indices[next_seq[i][t]]] = 1
+
+        self.text = text
+        self.chars = chars
+        self.char_indices = char_indices
+        self.indices_char = indices_char
+        self.sentences = sentences
+        self.next_chars = next_seq
+        self.X = x
+        self.Y = y
